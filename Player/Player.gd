42d845421ac731e1a8d6d2bb2666b8c2eb4c9d3a
brent <brent@novastel.la>
2020-07-30 00:22:05 -0400

    update key mappings and player script

--- Player/Player.gd
+++ Player/Player.gd
@@ -10,7 +10,7 @@ enum {
 	ATTACK
 }
 
-var state
+var state = MOVE
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
@@ -21,6 +21,22 @@ func _ready():
 	animationTree.active = true
 
 func _physics_process(delta):
+	
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			attack_state(delta)
+
+# Function to handle attack state
+func attack_state(delta):
+	pass
+
+
+# Function to handle move state
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -35,4 +51,4 @@ func _physics_process(delta):
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	velocity = move_and_slide(velocity)
+	velocity = move_and_slide(velocity)	
